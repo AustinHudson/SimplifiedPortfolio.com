86f1e1dc6dd605cc3320bdd3e22a475e482f8a94
ADT\ahudson <ahudson@adt.com>
2018-10-22 10:04:59 -0500

    cleaned up dashboard route js

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -1,8 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-        host: 'http://localhost:3000',
-        namespace: 'api',
+        // host: 'http://localhost:3000',
+        // namespace: 'api',
 
 //         // ajaxOptions: function(url, type, options) {
 //         // var hash = this._super(url, type, options);
--- frontend/app/routes/dashboard.js
+++ frontend/app/routes/dashboard.js
@@ -1,9 +1,7 @@
 import Route from '@ember/routing/route';
 import $ from 'jquery';
-import { inject } from '@ember/service';
 
 export default Route.extend({
-    store: inject(),
 
     model() {        
         return $.ajax({
