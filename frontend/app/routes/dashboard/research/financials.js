f545133a3925fc4dc3f18be9c92f0d3bd5f3fd60
ADT\ahudson <ahudson@adt.com>
2018-10-29 16:57:38 -0500

    finished research financials section

--- frontend/app/routes/dashboard/research/financials.js
+++ frontend/app/routes/dashboard/research/financials.js
@@ -1,9 +1,23 @@
 import Route from '@ember/routing/route';
+import $ from 'jquery';
+
 
 export default Route.extend({
 
     model() {
         let { symbol } = this.paramsFor('dashboard.research');
         console.log(symbol);
+
+        const quarterlyURL = 'http://localhost:3000/api/quarterly_financials?symbol=' + symbol;
+
+        const quarterlyAPI = $.ajax({
+            url: quarterlyURL,
+            types: 'GET',
+            dataType: 'jsonp',    
+        });
+
+        console.log(quarterlyAPI);
+
+        return quarterlyAPI;   
     }
 });
