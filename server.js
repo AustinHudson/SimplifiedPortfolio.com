88b0093c5e0162f90f199d5b24c9459a48d5caec
ADT\ahudson <ahudson@adt.com>
2018-11-27 12:00:23 -0600

    changed port in server.js

--- server.js
+++ server.js
@@ -3,10 +3,7 @@ var express = require('express');
 var app = express();
 
 // set our port
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
-}
+var port = process.env.PORT || 3000;
 
 // set the static files location 
 app.use(express.static(__dirname + '/public'));
@@ -16,7 +13,7 @@ require('./NodeApp/routes')(app); // configure our routes
 // startup our app at http://localhost:3000
 app.listen(port);
 
-console.log('server running on http://localhost:' + port);
+console.log('server running on http://localhost:3000');
 
 // expose app
 exports = module.exports = app;
